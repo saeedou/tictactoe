45e15c53c2f818d197eaf3eb840b3c6a1d5a857c
Saeed Mahmoodi <saeed.m201@gmail.com>
2023-01-13 14:02:06 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,14 @@ dependencies = [
 
 setup(
     name=PACKAGENAME,
-    version= '0.1',
+    version= '0.1.0',
     description='tic tac toe game',
     long_description=open('README.md').read(),
     install_requires=dependencies,
     py_modules=[PACKAGENAME],
-
+    entry_points={
+        'console_scripts': [
+            'tictactoe = tictactoe:Game.quickstart',
+        ]
+    }
 )
